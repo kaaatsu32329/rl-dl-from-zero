26f4f0d13637bfc431e756a1a5483cc239c8d1be
katsu <katsu1112uchi@gmail.com>
2022-05-20 00:34:08 +0900

    Up

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # rl-dl-from-zero
 
 ゼロから作るDeep Learning 4 強化学習編のサンプルコードを読み進めながらRustで作った話。
+
+## 使い方
+
+```sh
+git clone https://github.com/kaaatsu32329/rl-dl-from-zero
+cargo run --bin [BIN_NAME]
+```
+
+## 構成
--- common/src/gridworld_render.rs
+++ common/src/gridworld_render.rs
@@ -1,4 +1,6 @@
-use iced::*;
+use iced::{
+    button, Alignment, Button, Column, Element, Sandbox, Settings, Text,
+};
 
 #[derive(Debug, Default)]
 struct AgentData {
@@ -8,10 +10,14 @@ struct AgentData {
     wall_state: (i32, i32),
     ys: usize,
     xs: usize,
+    reset_button: button::State,
+    count: usize,
 }
 
-#[derive(Debug, Clone)]
-enum Message {}
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    ResetButton,
+}
 
 impl Sandbox for AgentData {
     type Message = Message;
@@ -30,6 +36,8 @@ impl Sandbox for AgentData {
                 wall_state: wall_state,
                 ys: reward_map.len(),
                 xs: reward_map[0].len(),
+                reset_button: button::State::default(),
+                count: 0,
             },
             Command::none(),
         )
@@ -41,6 +49,9 @@ impl Sandbox for AgentData {
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
+            Message::ResetButton => {
+                self.count += 1;
+            }
             _ => {
                 None;
             }
@@ -48,19 +59,26 @@ impl Sandbox for AgentData {
     }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
-        let content = Column::new();
-
-        Container::new(content)
-            .width(Length::Fill)
-            .height(Length::Fill)
-            .style(style::Container)
+        Column::new()
+            .padding(20)
+            .align_items(Alignment::Center)
+            .push(
+                iced::Button::new(&mut self.reset_button, Text::new("Reset"))
+                    .on_press(Message::ResetButton),
+            )
             .into()
+
+        // Container::new(content)
+        //     .width(Length::Fill)
+        //     .height(Length::Fill)
+        //     .style(style::Container)
+        //     .into()
     }
 }
 
 fn main() -> iced::Result {
-    AgentData::run(Settings {
-        antialiasing: true,
-        ..Settings::default()
-    })
+    // AgentData::run(Settings {
+    //     antialiasing: true,
+    //     ..Settings::default()
+    // })
 }
